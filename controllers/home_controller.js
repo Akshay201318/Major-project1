d08c8bbd8248f50b3fa60a58960217ab1b6def67
Akshay <akshaypanchal201318@gmail.com>
2020-07-23 00:44:21 +0530

    added a router and a controller for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home=function(req,res)
+{
+    return res.end('<h1> My home page</h1>');
+}
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//using the router
+app.use('/',require('./routes'));
+
 app.listen(port,function(err){
            if(err)
            {
